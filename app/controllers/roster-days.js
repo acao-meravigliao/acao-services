4b3531d050f88e94d3384595718ae77d14427840
Daniele Orlandi <daniele@orlandi.com>
2017-11-22 01:54:32 +0100

    Added sorting for roster days

--- app/controllers/roster-days.js
+++ app/controllers/roster-days.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  rosterDaysSortOrder: [ 'date' ],
+  sortedRosterDays: Ember.computed.sort('model', 'rosterDaysSortOrder'),
+
   prevYear: Ember.computed('currentYear', function() { return this.get('currentYear') - 1; }),
   nextYear: Ember.computed('currentYear', function() { return this.get('currentYear') + 1; }),
 });
