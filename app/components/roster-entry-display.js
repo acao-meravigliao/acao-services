98c40ec8f7d9a2d5ccd739509f4c05099c61a74a
Daniele Orlandi <daniele@orlandi.com>
2018-04-26 18:18:37 +0200

    Preliminary implementation of rosterEntry exchange

--- app/components/roster-entry-display.js
+++ app/components/roster-entry-display.js
@@ -1,6 +1,36 @@
-
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   tagName: '',
+
+  ajax: service(),
+
+  actions: {
+    onPopupShow(value, popup) {
+      this.set('value', new Date());
+      this.set('currentPopup', popup);
+    },
+    doExchange() {
+      //this.set('entry.on_offer_since', new Date());
+      this.get('ajax').request('/ygg/acao/roster_entries/' + this.get('entry').get('id').toString() + '/offer', {
+        method: 'POST',
+        data: {},
+      });
+
+      this.get('currentPopup').hide();
+    },
+    doCancelExchange() {
+      //this.set('entry.on_offer_since', new Date());
+      this.get('ajax').request('/ygg/acao/roster_entries/' + this.get('entry').get('id').toString() + '/offer_cancel', {
+        method: 'POST',
+        data: {},
+      });
+
+      this.get('currentPopup').hide();
+    },
+    cancel(){
+      this.get('currentPopup').hide();
+    },
+  },
 });
