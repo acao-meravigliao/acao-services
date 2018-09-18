6d4cc2ccb2a17c81a20f1030254676650ca3380e
Daniele Orlandi <daniele@orlandi.com>
2018-09-18 02:39:51 +0200

    Added lint ignores for uninteresting or TODO rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'no-bare-strings': false,
+    'attribute-indentation': false,
+    'block-indentation': false,
+    'self-closing-void-elements': false,
+
+    'no-html-comments': false, // For development
+    'no-inline-styles': false, // For development
+    'no-unused-block-params': false, // For development
+
+    'no-invalid-interactive': false, // TODO
+    'no-partial': false, // TODO
+  },
 };
