10543f7a49ad642f086b3fac9224c0c12864c4da
Daniele Orlandi <daniele@orlandi.com>
2018-10-17 05:39:24 +0200

    Removed unused dependency

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ember build -prod && rsync -v --recursive --delete --delete-excluded dist/ yggdra@iserver.acao.it:/opt/acao-services/
-ember build && rsync -v --recursive --delete --delete-excluded dist/ yggdra@iserver.acao.it:/opt/acao-services/
-
