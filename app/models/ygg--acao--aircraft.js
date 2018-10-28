1b11c4ecd1a4054fadb25f6c09d236989b2e4e43
Daniele Orlandi <daniele@orlandi.com>
2018-10-28 17:02:47 +0100

    Expanded some template with more data

--- app/models/ygg--acao--aircraft.js
+++ app/models/ygg--acao--aircraft.js
@@ -6,8 +6,12 @@ export default DS.Model.extend({
   race_registration: DS.attr('string'),
   flarm_identifier: DS.attr('string'),
   icao_identifier: DS.attr('string'),
+  serial_number: DS.attr('string'),
+  hangar: DS.attr('boolean'),
+  arc_valid_to: DS.attr('date'),
+  insurance_valid_to: DS.attr('date'),
 
-  aircraft_type: DS.belongsTo('ygg--core--person'),
+  aircraft_type: DS.belongsTo('ygg--acao--aircraft-type'),
+  club: DS.belongsTo('ygg--acao--club'),
   owner: DS.belongsTo('ygg--core--person'),
-
 });
