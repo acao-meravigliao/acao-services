25ab2b30ec88a98b5f7e2fdca160a8ca08138954
Daniele Orlandi <daniele@orlandi.com>
2018-10-28 17:06:55 +0100

    Various fixes and improvements to invoice model

--- app/models/ygg--acao--invoice--detail.js
+++ app/models/ygg--acao--invoice--detail.js
@@ -1,8 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  invoice_id: DS.attr('number'),
+  service_type_id: DS.attr('number'),
   price: DS.attr('number'),
-  extra_info: DS.attr('string'),
+  descr: DS.attr('string'),
+  count: DS.attr('number'),
+  specific_data: DS.attr('string', { key: 'data' }),
 
   invoice: DS.belongsTo('ygg--acao--invoice'),
   service_type: DS.belongsTo('ygg--acao--service-type'),
