1d4aa1f1fdce801e8e2fdca86f9413094779baf9
Daniele Orlandi <daniele@orlandi.com>
2018-10-28 17:08:25 +0100

    Various fixes and improvements to token transactions

--- app/models/ygg--acao--token-transaction.js
+++ app/models/ygg--acao--token-transaction.js
@@ -2,9 +2,15 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   uuid: DS.attr('string'),
+  person_id: DS.attr('number'),
+  aircraft_id: DS.attr('number'),
   recorded_at: DS.attr('date'),
   descr: DS.attr('string'),
   amount: DS.attr('number'),
   prev_credit: DS.attr('number'),
   credit: DS.attr('number'),
+  old_operator: DS.attr('string'),
+  old_marche_mezzo: DS.attr('string'),
+
+  aircraft: DS.belongsTo('ygg--acao--aircraft'),
 });
