166b40645652bdde48b3df69bd7be1344df33b7d
Daniele Orlandi <daniele@orlandi.com>
2018-10-28 17:10:03 +0100

    Temporarily disable store.query() cache

--- app/services/store.js
+++ app/services/store.js
@@ -10,20 +10,20 @@ export default DS.Store.extend({
 
 console.log("STORE QUERY", modelName, query);
 
-    if (typeof(adapter.shouldReloadQuery) === 'function' && !adapter.shouldReloadQuery(modelName, { filter: query.filter })) {
-console.log("STORE QUERY CAN FILTER WITHOUT QUERYING", modelName, query);
-
-      let array = this.peekAll(normalizedModelName);
-      let filter = query.filter;
-      if (filter) {
-        array = array.filter((item) => (
-          Object.keys(filter).every((key) => (item.get(key) == filter[key]))
-        ));
-      }
-
-      return DS.PromiseArray.create({ promise: Promise.resolve(array) });
-    } else {
+//    if (typeof(adapter.shouldReloadQuery) === 'function' && !adapter.shouldReloadQuery(modelName, { filter: query.filter })) {
+//console.log("STORE QUERY CAN FILTER WITHOUT QUERYING", modelName, query);
+//
+//      let array = this.peekAll(normalizedModelName);
+//      let filter = query.filter;
+//      if (filter) {
+//        array = array.filter((item) => (
+//          Object.keys(filter).every((key) => (item.get(key) == filter[key]))
+//        ));
+//      }
+//
+//      return DS.PromiseArray.create({ promise: Promise.resolve(array) });
+//    } else {
       return this._super(...arguments);
-    }
+//    }
   },
 });
