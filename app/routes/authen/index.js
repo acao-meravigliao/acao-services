cb76a032dc9469e3c8ccc8ff158a271a5165deb5
Daniele Orlandi <daniele@orlandi.com>
2018-12-28 15:11:31 +0100

    Fixed missing session in tow-roster and index route

--- app/routes/authen/index.js
+++ app/routes/authen/index.js
@@ -1,10 +1,15 @@
 import Route from '@ember/routing/route';
 import { hash, all } from 'rsvp';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+
   model() {
     return hash({
-      rosterEntries: this.store.query('ygg--acao--roster-entry', { filter: { person_id: this.get('session.personId') } }).then((items) => {
+      rosterEntries: this.store.query('ygg--acao--roster-entry',
+                       { filter: { person_id: this.get('session.personId') } },
+                       { adapterOptions: { view: 'with_days' } }).then((items) => {
         return all(items.map((l) => l.get('roster_day'))).then(() => items);
       }),
     });
--- app/routes/authen/tow-roster/select.js
+++ app/routes/authen/tow-roster/select.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+
   model(params) {
     return hash({
       rosterEntries: this.store.query('ygg--acao--tow-roster-entry', { filter: { person_id: this.get('session.personId') } }),
