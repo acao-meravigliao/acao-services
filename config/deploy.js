d4f67268449f996887294957363c6b4163e35b59
Daniele Orlandi <daniele@orlandi.com>
2019-01-14 14:32:11 +0100

    Added git-tag-commit deployment plugin

--- config/deploy.js
+++ config/deploy.js
@@ -40,6 +40,13 @@ module.exports = function(deployTarget) {
     }
   }
 
+  ENV["revision-data"] = {
+    type: 'git-tag-commit',
+//    scm: function(context) {
+//      return require('./lib/scm-data-generators')['git'];
+//    }
+  }
+
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
   // ENV object synchronously.
