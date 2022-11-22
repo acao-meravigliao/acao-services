eee9126cddf9dcb3d078e9b1373be7cec25589ea
Daniele Orlandi <daniele@orlandi.com>
2022-11-22 20:00:43 +0100

    Implemented idle timeout

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,4 +3,5 @@ import { service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
+  @service session;
 }
--- app/services/session.js
+++ app/services/session.js
@@ -16,6 +16,7 @@ class ServerError extends RemoteException { }
 
 export default class SessionService extends Service.extend(Evented) {
   @service store;
+  @service visibility;
   //@service('vihai-object-streaming') vos;
 
   @tracked is_authenticated = false;
@@ -24,7 +25,33 @@ export default class SessionService extends Service.extend(Evented) {
   @tracked person_id = null;
   @tracked person = null;
 
+  @tracked active = false;
+  @tracked expires = null;
+  @tracked last_action = null;
+
   @tracked is_loaded = false;
+
+  constructor() {
+    super(...arguments);
+
+    this.visibility.on('change', () => {
+      if (this.visibility.visible) {
+        this.activity();
+      }
+    });
+
+    let cb = this.activity.bind(this);
+
+    document.ontouchstart = cb;
+    document.onclick = cb;
+    document.onkeydown = cb;
+
+    window.addEventListener('beforeunload', () => {
+      if (this.active && this.expires) {
+        this.logout();
+      }
+    });
+  }
 
   async load() {
     let res;
@@ -102,6 +129,8 @@ export default class SessionService extends Service.extend(Evented) {
 
       let json = await res.json();
 
+      this.last_reported_activity = new Date();
+
       await this.update(json);
 
       if (json.authenticated) {
@@ -148,6 +177,8 @@ export default class SessionService extends Service.extend(Evented) {
       }
 
       let json = await res.json();
+
+      this.last_reported_activity = new Date();
 
       await this.update(json);
 
@@ -197,6 +228,8 @@ export default class SessionService extends Service.extend(Evented) {
 
       let json = await res.json();
 
+      this.last_reported_activity = new Date();
+
       await this.update(json);
 
       if (json.authenticated) {
@@ -219,6 +252,8 @@ export default class SessionService extends Service.extend(Evented) {
 
   async logout() {
     console.log('LOGOUT...');
+
+    this.idle_timer_stop();
 
     let abc = new AbortController();
     setTimeout(() => abc.abort(), 5000);
@@ -305,13 +340,28 @@ export default class SessionService extends Service.extend(Evented) {
     }
   }
 
+  activity() {
+    this.last_activity = new Date();
+
+    if (this.active && this.expires) {
+      if (!this.last_reported_activity || ((this.last_activity - this.last_reported_activity) > 60000)) {
+        this.refresh();
+      } else {
+        this.idle_timer_start();
+      }
+    }
+  }
+
   async update(session_data) {
     let old_authenticated = this.is_authenticated;
 
     this.session_id = session_data.id;
+    this.is_authenticated = session_data.authenticated;
     this.capabilities = session_data.capabilities;
     this.auth_method = session_data.auth_method;
-    this.is_authenticated = session_data.authenticated;
+
+    this.active = session_data.active;
+    this.expires = session_data.expires ? new Date(session_data.expires) : null;
 
     if (!old_authenticated && session_data.authenticated) {
       this.person_id = session_data.auth_person.id;
@@ -325,21 +375,49 @@ export default class SessionService extends Service.extend(Evented) {
       this.trigger('session_becomes_not_authenticated', arguments);
     }
 
-    if (session_data.expires) {
-      this.refresh_timer = later(this, () => {
-        try {
-          fetch('/ygg/session/refresh', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8',
-              'Accept': 'application/json',
-            },
-            body: JSON.stringify({}),
-          });
-        } catch(e) {
-          console.error("Session refresh error:", e);
-        }
-      }, 60000); // FIXME, calculate proper refresh time
+    if (this.expires) {
+      this.activity();
     }
+  }
+
+  idle_timer_start() {
+    let timeout = this.expires - new Date();
+
+    this.idle_timer_stop();
+
+    this.idle_timer = later(this, this.idle_timer_fired, timeout);
+  }
+
+  idle_timer_stop() {
+    if (this.idle_timer) {
+      cancel(this.idle_timer);
+      this.idle_timer = null;
+    }
+  }
+
+  idle_timer_fired() {
+    this.logout();
+  }
+
+  refresh() {
+    this.last_reported_activity = new Date();
+
+    fetch('/ygg/session/refresh', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({}),
+    }).then((res) => {
+      if (res.headers.get('content-type').startsWith('application/json'))
+        return res.json().then((json) => ([res, json]));
+      else
+        throw new ServerFailure;
+    }).then(([ res, json ]) => {
+      this.update(json);
+    }).catch((e) => {
+      console.error("Session refresh error:", e);
+    });
   }
 }
