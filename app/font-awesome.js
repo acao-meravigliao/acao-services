87487f12dfc02fbba71e9f9de5d9aa0a66073de9
Daniele Orlandi <daniele@orlandi.com>
2025-05-16 14:50:21 +0200

    Updates for font-awesome

---
+++ app/font-awesome.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import * as freeSolidSvgIcons from '@fortawesome/free-solid-svg-icons';
+
+console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", freeSolidSvgIcons);
+
+library.add(freeSolidSvgIcons['fas']);
