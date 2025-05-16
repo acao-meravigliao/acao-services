9345f40793c17bf403c0b6ea5ffcc3593972d561
Daniele Orlandi <daniele@orlandi.com>
2025-05-16 14:51:47 +0200

    Changed user for deployment on fe.acao.it

--- config/deploy.js
+++ config/deploy.js
@@ -50,7 +50,7 @@ module.exports = function(deployTarget) {
     ENV.build.environment = 'production';
 
     ENV['rsync-assets'] = {
-      destination: 'yggdra@fe.acao.it:/opt/acao-services/',
+      destination: 'acaofe.acao.it:/opt/acao-services/',
       source: 'dist/',
       excludeIndexHTML: false,
       ssh: true,
