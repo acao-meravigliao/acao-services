4d6d53511b1dcc97c3033b835b1d204e1dacb879
Daniele Orlandi <daniele@orlandi.com>
2025-05-27 00:10:50 +0200

    Ported to Embroider 4.x

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,15 @@
-'use strict';
-
+'use strict';;
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
+const {
+  compatBuild
+} = require("@embroider/compat");
+
+module.exports = async function(defaults) {
+  const {
+    buildOnce
+  } = await import("@embroider/vite");
+
   let app = new EmberApp(defaults, {
     sassOptions: {
       includePaths: [
@@ -26,34 +33,12 @@ module.exports = function (defaults) {
       },
     },
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  //return app.toTree();
-
-  const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack, {
+  return compatBuild(app, buildOnce, {
     staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
     staticHelpers: true,
     staticModifiers: true,
     staticComponents: true,
-    staticEmberSource: true,
-    skipBabel: [
-      {
-        package: 'qunit',
-      },
-    ],
+    staticEmberSource: true
   });
 };
