02989eedafe09fd820c9239e1937d6ca38d1e9e6
Daniele Orlandi <daniele@orlandi.com>
2025-09-11 15:57:43 +0200

    Fixed duration helper

--- app/helpers/format-duration.js
+++ app/helpers/format-duration.js
@@ -1,5 +1,12 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function formatDuration([dur]) {
-  return dur ? `${(dur / 3600000).toFixed().padStart(2, '0')}:${((dur % 3600000) / 60000).toFixed().padStart(2, '0')}:${((dur % 60000) / 1000).toFixed().padStart(2, '0')}` : '';
+export default helper(function formatDuration([dur, secs]) {
+  if (!dur)
+    return '';
+
+  let hh = Math.floor(dur / 3600000).toFixed().padStart(2, '0');
+  let mm = Math.floor((dur % 3600000) / 60000).toFixed().padStart(2, '0');
+  let ss = Math.floor((dur % 60000) / 1000).toFixed().padStart(2, '0');
+
+  return `${hh}:${mm}` + (secs ? `:${ss}` : '');
 });
