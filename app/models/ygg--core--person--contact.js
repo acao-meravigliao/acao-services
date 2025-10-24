f88653172383227ae2167fde1db050763107a76a
Daniele Orlandi <daniele@orlandi.com>
2025-10-24 17:53:54 +0200

    Moved email validation to person email

--- app/models/ygg--core--person--contact.js
+++ app/models/ygg--core--person--contact.js
@@ -5,4 +5,14 @@ export default class YggCorePersonContactModel extends Model {
   @attr('string') type;
   @attr('string') value;
   @attr('string') descr;
+
+  get is_valid() {
+    switch(this.type) {
+    case 'phone':
+    case 'mobile':
+      return this.value.trim().match(/^[\+]?[-0-9() ]+$/im);
+    default:
+      return true;
+    }
+  }
 }
--- app/models/ygg--core--person--email.js
+++ app/models/ygg--core--person--email.js
@@ -6,4 +6,10 @@ export default class YggCorePersonEmailModel extends Model {
   @attr('string') descr;
 
   @vosBelongsTo('email', 'ml_address') ml_address;
+
+  get is_valid() {
+    return this.value.toLowerCase().match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  }
 }
